2bb6f74885d6653f01cfcf08fef16b004dab7189
Eric Larson <ericlarsonbroom@gmail.com>
2020-04-22 10:06:19 -0600

    created ingredient mehtod and test

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -8,5 +8,14 @@ class CookBook
     @recipes << recipe
   end
 
+  def ingredients
+
+    cook_book_ingredients = recipes.flat_map do |recipie|
+      recipie.ingredients_required.keys
+    end.uniq
+    ingredient_names = cook_book_ingredients.map do |ingredient|
+      ingredient.name
+    end
+  end
 
 end
